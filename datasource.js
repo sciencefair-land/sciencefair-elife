f992bf99e5b3285ff229da1a195cbdbdd806f8f4
Richard Smith-Unna <richardsmith404@gmail.com>
2016-06-07 14:25:39 +0100

    Queues

--- datasource.js
+++ datasource.js
@@ -3,46 +3,63 @@ var fs = require('fs')
 var untildify = require('untildify')
 var path = require('path')
 var mkdirp = require('mkdirp')
+var queue = require('queue')
 
 // only sync the full-sized JPG, and the full article PDF and XML
-var filepattern = /v[0-9]+\.(jpg|pdf|xml)/
+function keepfile (path) {
+  return /v[0-9]+\.(jpg|pdf|xml)/.test(path) && (path.indexOf('figures') === -1)
+}
 
 function sync (cb) {
   var localdir = untildify('~/.sciencefair/data/elife/')
   mkdirp.sync(localdir)
 
+  var q = queue({ concurrency: 1 })
+  var filestosync = []
+  var q2 = queue({ concurrency: 1 })
+
+  q.on('end', function (err) {
+    if (err) console.trace(err)
+    console.log('All metadata synced')
+    console.log('Processing', filestosync.length, 'items in download queue')
+    filestosync.forEach(function (pair) {
+      var remote = pair[0]
+      var local = pair[1]
+      q2.push(function (next) {
+        s3bucket.syncFile(remote, local, next)
+      })
+    })
+    q2.start()
+  })
+
+  q2.on('end', function (err) {
+    if (err) console.trace(err)
+    console.log('All downloads completed')
+  })
+
+  console.log('Fetching bucket metadata')
   s3bucket.getPrefixes(function (err, prefixes) {
     if (err) cb(err)
-
-    // prefixes.forEach(syncArticle)
-    syncArticle(prefixes[0].Prefix[0])
+    console.log('Found', prefixes.length, 'articles')
+    prefixes.forEach(function (resp) {
+      syncArticle(resp.Prefix[0])
+    })
+    if (!q.running) q.start()
   })
 
   function syncArticle (prefix) {
-    mkdirp.sync(path.join(localdir, prefix.replace(/^\//, '')))
-    s3bucket.listPrefix(prefix, function (err, listing) {
-      if (err) cb(err)
-      var objects = listing.map(parseEntry)
-      var i = 0
-
-      var iterator
-
-      iterator = function (err) {
-        if (err) return cb(err)
-        // emit filesynced
-        i += 1
-        if (i < objects.length) {
-          if (filepattern.test(objects[i].path)) {
-            syncFile(objects[i], iterator)
-          } else {
-            iterator()
+    q.push(function (nextprefix) {
+      mkdirp.sync(path.join(localdir, prefix.replace(/^\//, '')))
+      s3bucket.listPrefix(prefix, function (err, listing) {
+        // console.log('Syncing article', prefix)
+        if (err) cb(err)
+        listing.map(parseEntry).forEach(function (entry) {
+          if (keepfile(entry.path)) {
+            syncFile(entry)
           }
-        } else {
-          cb()
-        }
-      }
-
-      syncFile(objects[i], iterator)
+        })
+        nextprefix()
+      })
     })
   }
 
@@ -53,21 +70,20 @@ function sync (cb) {
     }
   }
 
-  function syncFile (entry, donefn) {
-    console.log('syncing file', entry.path)
+  function syncFile (entry) {
     var localpath = path.join(localdir, entry.path)
     fs.stat(localpath, function (err, stat) {
+      // console.log('Checking file', entry.path)
       if (err) {
-        console.log('local file missing. downloading...')
-        s3bucket.syncFile(entry.path, localpath, donefn)
+        // console.log('Local file missing. download queued...')
+        filestosync.push([entry.path, localpath])
       } else {
         var localmod = new Date(stat.mtime)
         if (entry.modified > localmod) {
-          console.log('remote file is newer. updating...')
-          s3bucket.syncFile(entry.path, localpath, donefn)
+          // console.log('Remote file is newer. download queued...')
+          filestosync.push([entry.path, localpath])
         } else {
-          console.log('no update needed')
-          donefn()
+          // console.log('No upda1wte needed')
         }
       }
     })
