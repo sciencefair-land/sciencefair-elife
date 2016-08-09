cd90a38dd172cb5647c83c706d86e6b54b9305c2
Richard Smith-Unna <richardsmith404@gmail.com>
2016-08-09 15:14:35 +0300

    New approach - get xml from github

--- datasource.js
+++ datasource.js
@@ -1,96 +1,105 @@
-var s3bucket = require('./s3bucket.js')
 var fs = require('fs')
 var untildify = require('untildify')
 var path = require('path')
-var mkdirp = require('mkdirp')
+var mkdirp = require('mkdirp').sync
 var queue = require('queue')
-var moment = require('moment')
 var _ = require('lodash')
-var exists = require('exists-file').sync
+var dl = require('download')
+var unzip = require('unzip')
+var glob = require('matched').sync
+var select = require('beau-selector')
 
-function sync (cb) {
-  var localdir = untildify('~/.sciencefair/data/elife/')
-  mkdirp.sync(localdir)
+var xmlzip = 'https://github.com/elifesciences/elife-article-xml/archive/master.zip'
+var listingURL = 'https://elife-publishing-cdn.s3.amazonaws.com/'
+var dir = untildify('~/.sciencefair/data/elife')
+mkdirp(dir)
 
-  var q = queue({ concurrency: 1 })
+var q = queue()
 
-  var syncFile = function (entry) {
-    if (/pdf$/.test(entry.path)) return false
-    var localpath = path.join(localdir, entry.path)
-    if (!exists(localpath)) {
-      q.push(function (next) {
-        s3bucket.syncFile(entry.path, localpath, next)
-      })
-    }
-  }
-
-  q.on('end', function (err) {
-    if (err) console.trace(err)
-  })
-
-  q.on('error', function (err) {
-    console.trace(err)
-  })
-
-  console.log('Checking for cached listing')
-
-  var listingfile = path.join(localdir, 'listing.json')
-  fs.stat(listingfile, function (err, stat) {
-    if (err) {
-      console.log('No cached listing, downloading listing')
-      refresh()
-    } else {
-      if (moment(stat.mtime) >= moment().startOf('day')) {
-        console.log('Using cached listing')
-        var listing = JSON.parse(fs.readFileSync(listingfile, 'utf8'))
-        listing.forEach(syncFile)
-        q.start()
-      } else {
-        console.log('Cached listing outdated, refreshing')
-        refresh()
-      }
-    }
-  })
-
-  function refresh () {
-    s3bucket.list(parseEntry, keepfile, function (err, listing) {
-      if (err) cb(err)
-
-      saveListing(listing)
-
-      var done = _.after(listing.length, function () {
-        q.start()
-      })
-
-      listing.forEach(function (entry) {
-        if (keepfile(entry.path)) {
-          syncFile(entry, done)
-        } else {
-          done()
-        }
-      })
-    })
-  }
-
-  function saveListing (listing) {
-    fs.writeFileSync(listingfile, JSON.stringify(listing, null, 2))
-    console.log('Listing saved to JSON file')
-  }
-
-  // only sync the full-sized JPG, and the full article PDF and XML
-  function keepfile (entry) {
-    return /v[0-9]+\.(jpg|xml)/.test(entry.path)
-  }
-
-  function parseEntry (entry) {
-    return {
-      path: entry.Key[0],
-      modified: moment(entry.LastModified[0]).valueOf()
-    }
-  }
+function download (src, dst, cb) {
+  dl(src)
+    .pipe(fs.createWriteStream(dst))
+    .on('end', cb)
 }
 
-sync(function (err) {
+function unpack (zip, dst, cb) {
+  // unzip downloaded zip file and unpack it
+  // returning path of unpacked dir
+  fs.createReadStream(zip)
+    .pipe(unzip.Extract({ path: dst }))
+    .on('end', function () {
+      console.log('unpacked zip to', dst)
+      cb()
+    })
+    .on('error', cb)
+}
+
+function getarticles (dir) {
+  // read article filenames and return only the latest version
+  // article filename for each article
+  var articles = glob(['articles/*.xml'], { cwd: dir })
+  return _.map(_.groupBy(articles, stripVersion), function (vs) {
+    return vs.sort()[vs.length - 1] + '.xml'
+  })
+}
+
+function stripVersion (a) {
+  return a.split(/v[0-9]/)[0]
+}
+
+function extractimgpaths (xmlfile, cb) {
+  // open xml file and return array of paths to images from
+  // <graphic> elements
+  var xml = fs.readFileSync(xmlfile)
+  select(xml, {
+    query: '//graphic',
+    attribute: 'xlink:href'
+  }, cb)
+}
+
+function queuedownload (src, dst) {
+  q.push(function (cb) {
+    download(src, dst, cb)
+  })
+}
+
+function queueimgdownload (imgpath) {
+  var jpg = imgpath.replace('.tif', '.jpg')
+  var articleno = imgpath.split('-')[1]
+  var src = [listingURL, articleno, jpg].join('/')
+  // given an image path, download the image from
+  // s3 bucket
+  var dstdir = path.join(dir, 'articles', articleno)
+  mkdirp(dstdir)
+  var dst = path.join(dstdir, jpg)
+
+  queuedownload(src, dst)
+}
+
+function handlearticles (articles) {
+  var done = _.after(articles.length, function () {
+    q.start()
+  })
+  articles.forEach(function (article) {
+    extractimgpaths(article, function (imgpaths) {
+      imgpaths.forEach(queueimgdownload)
+      done()
+    })
+  })
+}
+
+function unpackxml (err) {
   if (err) throw err
-  console.log('all syncing done')
-})
+  unpack(
+    path.join(dir, 'articles.zip'),
+    path.join(dir, 'articles'),
+    function (err) {
+      if (err) throw err
+      var articles = getarticles(path.join(dir, 'articles'))
+      console.log('found', articles.length, 'articles')
+      handlearticles(articles)
+    }
+  )
+}
+
+download(xmlzip, path.join(dir, 'articles.zip'), unpackxml)
